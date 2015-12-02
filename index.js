2b6a30a8732ea37114170a26f64db2417778d26e
Нефальсифицированный Хрюндель <honest-hrundel@yandex.ru>
2015-12-03 01:01:02 +0300

    :pig: Thu, 03 Dec 2015 01:01:02 +0300

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('table')
+        .removeClass('display')
+        .addClass('table table-striped table-bordered')
+        .DataTable({
+            paging: false,
+            order: [ 11, 'desc' ]
+        })
+
+    $('main').show();
+
+    $('[data-toggle="tooltip"]').tooltip()
+});
